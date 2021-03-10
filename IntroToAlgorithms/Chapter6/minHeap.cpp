87e37adfe1ef2736c3583d5a45b510757222a227
OctavioAB <octavio.anino@gmail.com>
2021-03-09 19:58:23 -0500

    created missing Ch. 6 directory. Renamed heapSort.cpp

---
+++ IntroToAlgorithms/Chapter6/minHeap.cpp
@@ -0,0 +1,63 @@
+/* Last edited on March 9, 2021
+  Octavio E Anino Barber
+A simple implementation of a Min-Binary Heap, based on the explanation given in 
+the book Introduction To Algorithms, by Thomas H. Cormen et al.
+*/
+
+
+using namespace std;
+
+int minHeapify(int[]* A, int A_len, int i);
+int left_ify(int i), right_ify(int i);
+int buildMinHeap(int[]* A, int A_len);
+
+int main(){
+  int arrayLength = 10;
+  int* array_1 = (int*)(calloc(sizeof(int)*arrayLength));
+  printArray(array_1,arrayLength);
+}
+
+int minHeapify(int* A, int heapSize, int i){ // TEST!
+  int l = left_ify(i);
+  int r = right_ify(i);
+  int smallest, temp;
+  if(l <= heapSize && A[l]<A[i]){
+    smallest = l;
+  } else {
+    smallest = i;
+  }
+  if(r <= heapSize && A[r]<A[smallest]){
+    smallest = r;
+  }
+  if(smallest!=i){
+    temp = A[i];
+    A[i]=A[smallest];
+    A[smallest]=temp;
+    minHeapify(A, heapSize, smallest);
+  }
+  return 1;
+}
+
+int buildMinHeap(int* A, int A_len){
+  int heapSize = A_len;
+  for(int i=(A_len/2)-1;i>=0;i--){
+    minHeapify(A, heapSize, i);
+  }
+}
+
+int left_ify(int i){
+  if(i==0) return 1;
+  return i<<1;
+}
+
+int right_ify(int i){
+  if(i==0) return 2;
+  return i<<1 + 1;
+}
+
+void printArray(int* arr, int a_len){
+  for(int i=0;i<a_len;i++){
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+}
